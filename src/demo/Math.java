989294244cbee0a9f813527b62d5ecba9d3f53f6
BalajiRamudu <trbalajiramu@gmail.com>
2022-08-18 16:41:19 +0530

    added Math and Calculator Class

---
+++ src/demo/Math.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class Math 
+{
+ 
+
+	public static void add(int a, int b)
+	{
+	    System.out.println("Sum is :" + (a+b));
+	}
+	
+	public static void add(int a, int b, int c)
+	{
+	    System.out.println("Sum is :" + (a+b+c));
+	}
+	
+	public static void multiply(int a, int b)
+	{
+	    System.out.println("Product is :" + a*b);
+	}
+	
+	public static void main(String[] args) {
+		add(5,6);
+		add(5,6,7);
+		multiply(4,5);
+	}
+
+}
